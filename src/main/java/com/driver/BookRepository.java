ca2c66eb2b184032eeeb4113ce8f68ba0a28cda9
Pranav Patil <121039421+pranavpatil15198@users.noreply.github.com>
2023-04-21 12:15:37 +0530

    Update BookRepository.java

--- src/main/java/com/driver/BookRepository.java
+++ src/main/java/com/driver/BookRepository.java
@@ -5,37 +5,62 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class BookRepository {
-
-    public BookRepository(){
-        
+    private ArrayList<Book> booklist;
+    private int id;
+    public BookRepository() {
+        this.booklist = new ArrayList<>();
+        this.id=1;
     }
 
     public Book save(Book book){
-        return null;
+        book.setId(id++);
+        booklist.add(book);
+        return book;
     }
 
     public Book findBookById(int id){
-
+        for(Book book : booklist){
+            if(book.getId()==id){
+                return book;
+            }
+        }
         return null;
     }
 
     public List<Book> findAll(){
-        return null;
+        return booklist;
     }
 
     public void deleteBookById(int id){
-        return;
+        for (Book book:booklist){
+            if(book.getId()==id){
+                booklist.remove(book);
+            }
+        }
     }
 
     public void deleteAll(){
-        return;
+
+        booklist.clear();
     }
 
     public List<Book> findBooksByAuthor(String author){
-        return null;
+        List<Book> list = new ArrayList<>();
+        for(Book book : booklist){
+            if(book.getAuthor().equals(author)){
+                list.add(book);
+            }
+        }
+        return list;
     }
 
     public List<Book> findBooksByGenre(String genre){
-        return null;
+        List<Book> list = new ArrayList<>();
+        for(Book book : booklist){
+            if(book.getGenre().equals(genre)){
+                list.add(book);
+            }
+        }
+        return list;
     }
 }
