fe776986d008efff8d5fb8842f2edb1bf132f88d
Pranav Patil <121039421+pranavpatil15198@users.noreply.github.com>
2023-04-21 12:15:00 +0530

    Update Book.java

--- src/main/java/com/driver/Book.java
+++ src/main/java/com/driver/Book.java
@@ -9,8 +9,41 @@ public class Book {
      private String genre;
      
      private String author;
+     public Book(String name, String genre, String author) {
+          this.name = name;
+          this.genre = genre;
+          this.author = author;
+     }
 
-     public Book(){
+     public int getId() {
+          return id;
+     }
 
+     public void setId(int id) {
+          this.id = id;
+     }
+
+     public String getName() {
+          return name;
+     }
+
+     public void setName(String name) {
+          this.name = name;
+     }
+
+     public String getGenre() {
+          return genre;
+     }
+
+     public void setGenre(String genre) {
+          this.genre = genre;
+     }
+
+     public String getAuthor() {
+          return author;
+     }
+
+     public void setAuthor(String author) {
+          this.author = author;
      }
 }
