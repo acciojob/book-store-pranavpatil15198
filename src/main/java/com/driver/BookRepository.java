78813d3ef89c47636c4633f5b16a6d19610829db
Pranav Patil <121039421+pranavpatil15198@users.noreply.github.com>
2023-04-21 13:50:12 +0530

    Update BookRepository.java

--- src/main/java/com/driver/BookRepository.java
+++ src/main/java/com/driver/BookRepository.java
@@ -5,62 +5,65 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class BookRepository {
-    private ArrayList<Book> booklist;
-    private int id;
-    public BookRepository() {
-        this.booklist = new ArrayList<>();
-        this.id=1;
+
+    public BookRepository(){
+        
     }
+    HashMap<Integer,Book> bookDb = new HashMap<>();
 
     public Book save(Book book){
-        book.setId(id++);
-        booklist.add(book);
+
+        bookDb.put(book.getId(), book);
         return book;
     }
 
     public Book findBookById(int id){
-        for(Book book : booklist){
-            if(book.getId()==id){
-                return book;
-            }
-        }
-        return null;
+
+            return bookDb.get(id);
+
     }
 
     public List<Book> findAll(){
+        List<Book> booklist = new ArrayList<>();
+        for(Integer id: bookDb.keySet()) {
+            booklist.add(bookDb.get(id));
+        }
+
         return booklist;
     }
 
-    public void deleteBookById(int id){
-        for (Book book:booklist){
-            if(book.getId()==id){
-                booklist.remove(book);
-            }
-        }
+    public String deleteBookById(int id){
+
+        if(bookDb.containsKey(id)){
+            bookDb.remove(id);
+         }
+        return "book is deleted";
     }
 
-    public void deleteAll(){
-
-        booklist.clear();
+    public String deleteAll(){
+        bookDb.clear();
+        return "all books are deleted";
     }
 
     public List<Book> findBooksByAuthor(String author){
-        List<Book> list = new ArrayList<>();
-        for(Book book : booklist){
-            if(book.getAuthor().equals(author)){
-                list.add(book);
+        List<Book> bookAuthorList = new ArrayList<>();
+
+        for(Integer id: bookDb.keySet()){
+            if(bookDb.get(id).getAuthor().equals(author)){
+                bookAuthorList.add(bookDb.get(id));
             }
         }
-        return list;
+        return bookAuthorList;
     }
 
     public List<Book> findBooksByGenre(String genre){
-        List<Book> list = new ArrayList<>();
-        for(Book book : booklist){
-            if(book.getGenre().equals(genre)){
-                list.add(book);
+        List<Book> genreBookList = new ArrayList<>();
+
+        for(Integer id: bookDb.keySet()){
+            if(bookDb.get(id).getGenre().equals(genre)){
+                genreBookList.add(bookDb.get(id));
             }
         }
-        return list;
+        return genreBookList;
     }
 }
